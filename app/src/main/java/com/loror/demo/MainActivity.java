6a45cd00480d61f04c6732629fb71218effa97f9
Loror <Lorrer@126.com>
2018-06-08 14:32:25 +0800

    boot

--- app/src/main/java/com/loror/demo/MainActivity.java
+++ app/src/main/java/com/loror/demo/MainActivity.java
@@ -2,12 +2,59 @@ package com.loror.demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.loror.lororboot.annotation.Bind;
+import com.loror.lororboot.annotation.Click;
+import com.loror.lororboot.annotation.ItemClick;
+import com.loror.lororboot.bind.BindAbleItem;
+import com.loror.lororboot.startable.LororActivity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends LororActivity {
+
+    @Bind(id = R.id.textView)
+    String text = "绑定TextView显示";
+    @Bind(id = R.id.editText)
+    String doubleBindText = "绑定EditText内容";
+    @Bind(id = R.id.imageView, imagePlace = R.mipmap.ic_launcher)
+    String image = "http://img.taopic.com/uploads/allimg/120727/201995-120HG1030762.jpg";
+    @Bind(id = R.id.listView)
+    List<ListItem> listItems = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        for (int i = 0; i < 10; i++) {
+            ListItem item = new ListItem();
+            item.text = "第" + i + "行";
+            listItems.add(item);
+        }
+        notifyListDataChangeById(R.id.listView);
+    }
+
+    @Click(id = R.id.button)
+    public void buttonClick(View view) {
+        Toast.makeText(this, doubleBindText, Toast.LENGTH_SHORT).show();
+    }
+
+    @ItemClick(id = R.id.listView)
+    public void listViewClick(View view, int position) {
+        Toast.makeText(this, "第" + position + "行点击", Toast.LENGTH_SHORT).show();
+    }
+
+    class ListItem extends BindAbleItem {
+
+        @Bind(id = R.id.text)
+        public String text;
+
+        @Override
+        public int getLayout() {
+            return R.layout.item_list_view;
+        }
     }
 }
