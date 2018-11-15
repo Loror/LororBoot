2f387bb25f03c8fb0527be015c5dffb43710b9c6
Loror <Lorrer@126.com>
2018-11-15 14:16:04 +0800

    boot

---
+++ app/src/main/java/com/loror/demo/App.java
@@ -0,0 +1,10 @@
+package com.loror.demo;
+
+import com.loror.lororboot.LororApplication;
+
+public class App extends LororApplication {
+    @Override
+    protected Class<?> getIdClass() {
+        return R.id.class;
+    }
+}
