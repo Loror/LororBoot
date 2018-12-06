b8ce745433cef1dac714468a3621cdc215a37a70
loror <Lorrer@126.com>
2018-12-06 23:17:12 +0800

    Loror

--- lororboot/src/main/java/com/loror/lororboot/annotation/FrameCall.java
+++ lororboot/src/main/java/com/loror/lororboot/annotation/FrameCall.java
@@ -5,6 +5,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+// 框架中使用的方法，外部请勿调用
 @Retention(RetentionPolicy.CLASS)
 @Target({ElementType.METHOD})
 public @interface FrameCall {
--- lororboot/src/main/java/com/loror/lororboot/bind/BindAble.java
+++ lororboot/src/main/java/com/loror/lororboot/bind/BindAble.java
@@ -1,5 +1,7 @@
 package com.loror.lororboot.bind;
 
+import com.loror.lororboot.annotation.FrameCall;
+
 public interface BindAble {
     /**
      * 找到bind触发,返回值控制是否拦截首次绑定显示
@@ -9,6 +11,7 @@ public interface BindAble {
     /**
      * 查找Bind
      */
+    @FrameCall
     void updateBind(Object tag);
 
     /**
