039739ae9167ccb52850684640daa13350dabedd
Loror <lorrer@126.com>
2019-05-21 14:46:59 +0800

    Loror

--- app/src/main/java/com/loror/demo/ServerApi.java
+++ app/src/main/java/com/loror/demo/ServerApi.java
@@ -2,6 +2,7 @@ package com.loror.demo;
 
 import com.loror.lororUtil.http.Responce;
 import com.loror.lororboot.annotation.BaseUrl;
+import com.loror.lororboot.annotation.DefaultParams;
 import com.loror.lororboot.annotation.Param;
 import com.loror.lororboot.annotation.GET;
 import com.loror.lororboot.httpApi.Observable;
@@ -9,6 +10,7 @@ import com.loror.lororboot.httpApi.Observable;
 @BaseUrl("http://www.bejson.com/")
 public interface ServerApi {
     @GET("")
+    @DefaultParams(keys = "key", values = "123")//用于指定固定参数，key，value位置需一一对应
     Observable<Responce> getResult(@Param("id") String id);
     //支持返回类型原生responce（Responce），字符串（String），对象（将使用Json解释器生成对象）
     //请求类型@GET，@POST，@DELETE，@PUT
